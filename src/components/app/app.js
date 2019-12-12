db3c213ddae510c73f35491bb2ad70fe7884c081
arinabilous <bilousiryna6@gmail.com>
2019-12-12 18:44:06 +0200

    add Functional btn del

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import './app.css';
+import React, {Component} from 'react';
 
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
 import TodoList from "../todo-list";
 
 
-const App = () => {
-    const todoData = [
-        { label: 'Drink Coffee', important: false, id: 1 },
-        { label: 'Make Awesome App', important: true, id: 2 },
-        { label: 'Have a lunch', important: false, id: 3 }
-    ];
-    return (
-        <div className="main">
-            <AppHeader/>
-            <SearchPanel/>
-            <TodoList  todos={todoData}
-                       onDeleted={(id)=> console.log('del', id)}/>
-        </div>
-    );
+export default class App extends Component {
+    state = {
+        todoData: [
+            {label: 'Drink Coffee', important: false, id: 1},
+            {label: 'Make Awesome App', important: true, id: 2},
+            {label: 'Have a lunch', important: false, id: 3}
+        ]
+    };
+    deleteItem = (id) => {
+        this.setState(({todoData}) => {
+
+            const idx = todoData.findIndex((el) => el.id === id);
+
+            const newArray = [
+                ...todoData.slice(0, idx),
+                ...todoData.slice(idx + 1)
+            ];
+            return {
+                todoData: newArray
+            }
+        });
+    };
+
+    render() {
+
+        return (
+            <div className="main">
+                <AppHeader/>
+                <SearchPanel/>
+                <TodoList todos={this.state.todoData}
+                          onDeleted={this.deleteItem}/>
+            </div>
+        )
+    };
+
 };
-export default App;
